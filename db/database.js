7f3fe8e9e53059615a9e89842dc9cf4c39d895ed
Jonathan Cano <jonathancanofreta@gmail.com>
2025-05-21 13:49:15 +0200

    Fixed Pool issue exporting pool

--- db/database.js
+++ db/database.js
@@ -3,28 +3,14 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
-async function verificarConexion(reintentos = 5, delay = 2000) {
-  for (let intento = 1; intento <= reintentos; intento++) {
-    try {
-      const client = await pool.connect();
-      console.log('✅ Conectado a la Base de Datos');
-      client.release();
-      return;
-    } catch (err) {
-      console.error(`❌ Intento ${intento} de ${reintentos} - Error al conectarse a la Base de Datos:`, err.message);
+pool.connect()
+  .then(client => {
+    console.log('Conectado a la Base de Datos');
+    client.release();
+  })
+  .catch(err => console.error('Error al conectarse a la Base de Datos', err));
 
-      if (intento === reintentos) {
-        console.error('💥 No se pudo establecer conexión tras varios intentos. Cerrando aplicación.');
-        process.exit(1);
-      }
-
-      await new Promise(resolve => setTimeout(resolve, delay));
-      delay *= 2;
-    }
-  }
-}
-
-verificarConexion();
+module.exports =  pool;
